e18e9025b88f0b4e3fcc08c243a7f4923149e953
Ľubomír Varga <lubomir.varga@qpp.sk>
2019-06-10 08:24:41 +0200

    WIP Refactor main mojo
    Small refactoring of baseDir usage.
    Removed nesting of big code part and added logging to it.

--- src/main/java/com/jamosolutions/automator/JamoAutomatorMojo.java
+++ src/main/java/com/jamosolutions/automator/JamoAutomatorMojo.java
@@ -20,8 +20,10 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -50,130 +52,140 @@ public class JamoAutomatorMojo extends AbstractMojo {
 	//maven version plugin
 	//http://www.mojohaus.org/versions-maven-plugin/
 	//mvn versions:use-latest-versions -DallowSnapshots=true
-	@Parameter(defaultValue = "${project.basedir}")
-	private File baseDir;
+
+    @Parameter(readonly = true, defaultValue = "${project}")
+    private MavenProject project;
+	
 	@Parameter(defaultValue = "${suite}")
 	private String descriptor;
 
-	protected File getReportDirectory() {
-		File reportFolder = new File(baseDir.getAbsolutePath(), "target/surefire-reports");
+	protected File getReportDirectory(File baseDirAbsolutePath) {
+		File reportFolder = new File(baseDirAbsolutePath, "target/surefire-reports");
 		if (!reportFolder.exists()) {
 			getLog().info("creating report folder");
 			reportFolder.mkdirs();
 		}
-		return new File(baseDir.getAbsolutePath(), "target/surefire-reports");
+		return new File(baseDirAbsolutePath, "target/surefire-reports");
 	}
 
 	public void execute() throws MojoExecutionException {
 		if (descriptor == null) {
 			descriptor = "testsuite";
 		}
-		File testSuiteFile = new File(baseDir.getAbsolutePath() + "/src/main/resources", descriptor + ".xml");
-		if (testSuiteFile.exists()) {
-			try {
-				//parse the testsuite file
-				JAXBContext jaxbContext = JAXBContext.newInstance(TestSuite.class);
-				Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-				TestSuite testSuite = (TestSuite) jaxbUnmarshaller.unmarshal(testSuiteFile);
-				System.out.println(testSuite.getDevices().get(0).getUdid());
-				List<Execution> executions = new ArrayList<>();
-				//build the xml test suite document
-				//http://help.catchsoftware.com/display/ET/JUnit+Format
-				File reportFile = new File(getReportDirectory().getAbsolutePath(), "TEST-com.jamoautomator." + testSuite.getName() + ".xml");
-				DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-				DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-				Document doc = docBuilder.newDocument();
-				Element testsuiteElement = doc.createElement("testsuite");
-				doc.appendChild(testsuiteElement);
-				testsuiteElement.setAttribute("name", testSuite.getName());
-				Integer nbOfErrors = 0;
-				Integer nbOfFailures = 0;
-				List<String> loginResult = login(testSuite.getCredentials(), testSuite.getUrl());
-				String userKey = loginResult.get(0);
-				String accessToken = loginResult.get(1);
-				for (Device device : testSuite.getDevices()) {
-					int idx = 0;
-					for (TestCase testCase : device.getTestCases()) {
-						ResponseStringWrapper response = runTestCase(device, testCase, userKey, accessToken, testSuite.getUrl(), idx);
-						idx++;
-						getLog().info("" + response.isSuccess());
-						if (response.isSuccess()) {
-							executions.add(new Execution(response.getMessage(), device, testCase));
-						} else {
-							nbOfErrors++;
-							Element testcaseElement = doc.createElement("testcase");
-							testcaseElement.setAttribute("time", "" + 0);
-							testcaseElement.setAttribute("name", testCase.getName());
-							testcaseElement.setAttribute("classname", "com.jamosolutions." + testSuite.getName() + "." + device.getName());
-							Element errorElement = doc.createElement("error");
-							errorElement.setAttribute("message", response.getMessage());
-							testcaseElement.appendChild(errorElement);
-							testsuiteElement.appendChild(testcaseElement);
-						}
+		final File baseDirAbsolutePath = this.project.getBasedir().getAbsoluteFile();
+		final Log log = getLog();
+		final File testSuiteFile = new File(baseDirAbsolutePath + "/src/main/resources", descriptor + ".xml");
+
+		if (false == testSuiteFile.exists()) {
+			log.warn(
+					"There was no testsuite file found. Set proper \"suite\" file (by default testsuite.xml). Currently looking for file here: \"" +
+							testSuiteFile.getAbsolutePath() + "\". Going to do nothing."
+			);
+			return;
+		}
+		try {
+			//parse the testsuite file
+			JAXBContext jaxbContext = JAXBContext.newInstance(TestSuite.class);
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			TestSuite testSuite = (TestSuite) jaxbUnmarshaller.unmarshal(testSuiteFile);
+			System.out.println(testSuite.getDevices().get(0).getUdid());
+			List<Execution> executions = new ArrayList<>();
+			//build the xml test suite document
+			//http://help.catchsoftware.com/display/ET/JUnit+Format
+			File reportFile = new File(getReportDirectory(baseDirAbsolutePath).getAbsolutePath(), "TEST-com.jamoautomator." + testSuite.getName() + ".xml");
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+			Document doc = docBuilder.newDocument();
+			Element testsuiteElement = doc.createElement("testsuite");
+			doc.appendChild(testsuiteElement);
+			testsuiteElement.setAttribute("name", testSuite.getName());
+			Integer nbOfErrors = 0;
+			Integer nbOfFailures = 0;
+			List<String> loginResult = login(testSuite.getCredentials(), testSuite.getUrl());
+			String userKey = loginResult.get(0);
+			String accessToken = loginResult.get(1);
+			for (Device device : testSuite.getDevices()) {
+				int idx = 0;
+				for (TestCase testCase : device.getTestCases()) {
+					ResponseStringWrapper response = runTestCase(device, testCase, userKey, accessToken, testSuite.getUrl(), idx);
+					idx++;
+					log.info("" + response.isSuccess());
+					if (response.isSuccess()) {
+						executions.add(new Execution(response.getMessage(), device, testCase));
+					} else {
+						nbOfErrors++;
+						Element testcaseElement = doc.createElement("testcase");
+						testcaseElement.setAttribute("time", "" + 0);
+						testcaseElement.setAttribute("name", testCase.getName());
+						testcaseElement.setAttribute("classname", "com.jamosolutions." + testSuite.getName() + "." + device.getName());
+						Element errorElement = doc.createElement("error");
+						errorElement.setAttribute("message", response.getMessage());
+						testcaseElement.appendChild(errorElement);
+						testsuiteElement.appendChild(testcaseElement);
 					}
 				}
-				Date now = new Date();
-				long startMillis = now.getTime();
-				while (executions.size() > 0) {
-					for (Iterator<Execution> iterator = executions.iterator(); iterator.hasNext();) {
-						Execution execution = iterator.next();
-						if ((new Date().getTime()) - startMillis > execution.getTestCase().getTimeout() * 60 * 1000) {
-							iterator.remove();
-							nbOfErrors++;
+			}
+			Date now = new Date();
+			long startMillis = now.getTime();
+			while (executions.size() > 0) {
+				for (Iterator<Execution> iterator = executions.iterator(); iterator.hasNext();) {
+					Execution execution = iterator.next();
+					if ((new Date().getTime()) - startMillis > execution.getTestCase().getTimeout() * 60 * 1000) {
+						iterator.remove();
+						nbOfErrors++;
+						Element testcaseElement = doc.createElement("testcase");
+						long duration = ((new Date().getTime()) - startMillis) / 1000;
+						testcaseElement.setAttribute("time", "" + duration);
+						testcaseElement.setAttribute("name", execution.getTestCase().getName());
+						testcaseElement.setAttribute("classname", "com.jamosolutions." + testSuite.getName() + "." + execution.getDevice().getName());
+						Element errorElement = doc.createElement("error");
+						errorElement.setAttribute("message", "could not find any report within " + (duration / 60) + " minutes");
+						testcaseElement.appendChild(errorElement);
+						testsuiteElement.appendChild(testcaseElement);
+					} else {
+						Report report = getReport(execution.getExecutionId(), accessToken, testSuite.getUrl());
+						if (report != null) {
 							Element testcaseElement = doc.createElement("testcase");
-							long duration = ((new Date().getTime()) - startMillis) / 1000;
+							long duration = (report.getEndDate().getTime() - report.getCreationDate().getTime()) / 1000;
 							testcaseElement.setAttribute("time", "" + duration);
 							testcaseElement.setAttribute("name", execution.getTestCase().getName());
 							testcaseElement.setAttribute("classname", "com.jamosolutions." + testSuite.getName() + "." + execution.getDevice().getName());
-							Element errorElement = doc.createElement("error");
-							errorElement.setAttribute("message", "could not find any report within " + (duration / 60) + " minutes");
-							testcaseElement.appendChild(errorElement);
-							testsuiteElement.appendChild(testcaseElement);
-						} else {
-							Report report = getReport(execution.getExecutionId(), accessToken, testSuite.getUrl());
-							if (report != null) {
-								Element testcaseElement = doc.createElement("testcase");
-								long duration = (report.getEndDate().getTime() - report.getCreationDate().getTime()) / 1000;
-								testcaseElement.setAttribute("time", "" + duration);
-								testcaseElement.setAttribute("name", execution.getTestCase().getName());
-								testcaseElement.setAttribute("classname", "com.jamosolutions." + testSuite.getName() + "." + execution.getDevice().getName());
-								if (report.getStatus() != 0) {
-									nbOfFailures++;
-									Element failureElement = doc.createElement("failure");
-									failureElement.setAttribute("message", "The test case did not succeed.");
-									Text errorMessageDetail = doc.createTextNode("For more detail click " + testSuite.getUrl() + "/index.html?reportDetail="
-											+ report.getKeyString());
-									failureElement.appendChild(errorMessageDetail);
-									testcaseElement.appendChild(failureElement);
-								}
-								testsuiteElement.appendChild(testcaseElement);
-								iterator.remove();
+							if (report.getStatus() != 0) {
+								nbOfFailures++;
+								Element failureElement = doc.createElement("failure");
+								failureElement.setAttribute("message", "The test case did not succeed.");
+								Text errorMessageDetail = doc.createTextNode("For more detail click " + testSuite.getUrl() + "/index.html?reportDetail="
+										+ report.getKeyString());
+								failureElement.appendChild(errorMessageDetail);
+								testcaseElement.appendChild(failureElement);
 							}
+							testsuiteElement.appendChild(testcaseElement);
+							iterator.remove();
 						}
 					}
-					getLog().info("did not find report: will try again in 5 seconds...........");
-					Thread.sleep(5000);
 				}
-				testsuiteElement.setAttribute("errors", "" + nbOfErrors);
-				testsuiteElement.setAttribute("failures", "" + nbOfFailures);
-				long duration = ((new Date().getTime()) - startMillis) / 1000;
-				testsuiteElement.setAttribute("time", "" + duration);
-				TransformerFactory transformerFactory = TransformerFactory.newInstance();
-				Transformer transformer = transformerFactory.newTransformer();
-				DOMSource source = new DOMSource(doc);
-				StreamResult result = new StreamResult(reportFile);
-				transformer.transform(source, result);
-			} catch (JAXBException e) {
-				getLog().error("could not parse the descriptor file " + descriptor);
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ParserConfigurationException e1) {
-				getLog().error("could not build junit xml document");
-			} catch (TransformerException e) {
-				getLog().error("could not build junit xml document");
-			} catch (InterruptedException e) {
-				getLog().info("the test suite has been interrupted");
+				log.info("did not find report: will try again in 5 seconds...........");
+				Thread.sleep(5000);
 			}
+			testsuiteElement.setAttribute("errors", "" + nbOfErrors);
+			testsuiteElement.setAttribute("failures", "" + nbOfFailures);
+			long duration = ((new Date().getTime()) - startMillis) / 1000;
+			testsuiteElement.setAttribute("time", "" + duration);
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(reportFile);
+			transformer.transform(source, result);
+		} catch (JAXBException e) {
+			log.error("could not parse the descriptor file " + descriptor);
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e1) {
+			log.error("could not build junit xml document");
+		} catch (TransformerException e) {
+			log.error("could not build junit xml document");
+		} catch (InterruptedException e) {
+			log.info("the test suite has been interrupted");
 		}
 	}
 
